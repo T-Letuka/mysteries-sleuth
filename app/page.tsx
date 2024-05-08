6ea7a2a363dfc3224b1ee67070f121c98f274fe9
T-Letuka <tisetsoletuka@gmail.com>
2024-05-08 14:20:02 +0200

    setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ResponsiveNav from "./Components/ResponsiveNav";
+import About from "./Components/About";
+
+const page = () => {
+  return (
+    <div>
+      <ResponsiveNav />
+      <About />
+    </div>
+  );
+};
+
+export default page;
